e75ca5a60ef9b61d2239e5d976894a8ec2187e3a
Larry Luk <larryluk526@gmail.com>
2017-09-24 18:59:01 -0400

    setup alamofire

---
+++ Pokedex4/Model/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Pokedex4
+//
+//  Created by Larry Luk on 2017-09-24.
+//  Copyright © 2017 App App Studio. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
+
+
--- Pokedex4/Model/Pokemon.swift
+++ Pokedex4/Model/Pokemon.swift
@@ -7,37 +7,62 @@
 //
 
 import Foundation
-
-//class Pokemon {
-//
-//    private var _name : String!
-//    private var _pokedexID : Int!
-//
-//    var name : String {
-//        return _name
-//    }
-//
-//    var pokedexID : Int {
-//        return _pokedexID
-//    }
-//
-//    init(name: String, pokedexID: Int) {
-//        self._name = name
-//        self._pokedexID = pokedexID
-//    }
-//
-//}
+import Alamofire
 
 class Pokemon {
-    
-    var name : String
-    var pokedexID : Int
-    
-    init(name: String, pokedexID: Int) {
-        self.name = name
-        self.pokedexID = pokedexID
+
+    private var _name : String!
+    private var _pokedexID : Int!
+    private var _description : String!
+    private var _type : String!
+    private var _attack : String!
+    private var _defense : String!
+    private var _height : String!
+    private var _weight : String!
+    private var _nextEvoTxt : String!
+    private var _pokemonURL : String!
+
+    var name : String {
+        return _name
+    }
+
+    var pokedexID : Int {
+        return _pokedexID
     }
     
     
+
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+        self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(self._pokedexID)"
+    }
     
+    func downloadPokemonDetail(completed: DownloadComplete) {
+
+    }
+
 }
+
+//class Pokemon {
+//
+//    var name : String
+//    var pokedexID : Int
+//    var description : String
+//    var type : String
+//    var attack : String
+//    var defense : String
+//    var height : String
+//    var weight : String
+//    var nextEvoTxt : String
+//
+//
+//    init(name: String, pokedexID: Int) {
+//        self.name = name
+//        self.pokedexID = pokedexID
+//    }
+//
+//
+//
+//}
+
